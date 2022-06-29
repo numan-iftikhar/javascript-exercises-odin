593580fa49fe083cb0ecb44be213dbe9a2735f2c
numan-iftikhar <numaniftikhar300@gmail.com>
2022-06-29 20:12:07 +0500

    Complete leapYears test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  let isLeap = false;
 
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) isLeap = true;
+    } else isLeap = true;
+  }
+
+  return isLeap;
 };
 
 // Do not edit below this line
