c56e095842345034e109b17ca792a12ce1da5756
numan-iftikhar <numaniftikhar300@gmail.com>
2022-06-29 20:13:02 +0500

    Complete tempConversion test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,22 @@
-const ftoc = function() {
+const ftoc = function (fTemp) {
+  /*
+   * Math.round(result * 10) places decimal towards right one place.
+   * / 10 place decimal towards left one place.
+   * Hence, we get number rounded to one decimal place. :)
+   */
+  let cTemp = Math.round(((fTemp - 32) / 1.8) * 10) / 10;
 
+  return cTemp;
 };
 
-const ctof = function() {
+const ctof = function (cTemp) {
+  let fTemp = Math.round((1.8 * cTemp + 32) * 10) / 10;
 
+  return fTemp;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
