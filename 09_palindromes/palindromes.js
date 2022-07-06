5778e0a564c5dea61b37ecf514f65cd2feaf2995
numan-iftikhar <numaniftikhar300@gmail.com>
2022-07-06 19:54:52 +0500

    Complete palindromes test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let processedStr = str.toLowerCase().replace(/[^a-z]/g, "");
 
+  return processedStr.split("").reverse().join("") === processedStr;
+
+  /*
+    ALTERNATE: Using two pointer approach
+
+    let processedStr = str.toLowerCase().replace(/[^a-z]/g, "");
+
+    let s = 0;
+    let e = processedStr.length - 1;
+
+    while (s < e) {
+        if (processedStr[s] !== processedStr[e]) return false;
+        s++;
+        e--;
+    }
+    return true;
+  */
 };
 
 // Do not edit below this line
