0d7f114793b49614448bf2c9aeae3f676db2006e
numan-iftikhar <numaniftikhar300@gmail.com>
2022-07-07 16:48:24 +0500

    Complete getTheTitles test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,16 @@
-const getTheTitles = function() {
+const getTheTitles = function (array) {
+  return array.map((book) => book.title);
 
+
+  
+  /*
+    ! ALTERNATE
+  const titles = [];
+  titles.push(array[0].title);
+  titles.push(array[1].title);
+
+  return titles;
+  */
 };
 
 // Do not edit below this line
