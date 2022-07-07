532fa513ec3cab2b318f51830ed45cbed7a69dd2
numan-iftikhar <numaniftikhar300@gmail.com>
2022-07-07 16:48:01 +0500

    Complete fibonacci test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (pos) {
+  // check for edge cases
+  if (pos < 0) return "OOPS";
+  if (pos === 0) return 0;
 
+  let a = 0;
+  let b = 1;
+  let next = a + b;
+
+  for (let i = 1; i < pos - 1; i++) {
+    a = b;
+    b = next;
+    next = a + b;
+  }
+
+  return next;
 };
-
+console.log(fibonacci(6));
 // Do not edit below this line
 module.exports = fibonacci;
